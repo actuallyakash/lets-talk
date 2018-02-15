13d4fe76f2ef6cb3e5625cf3eceab575c9933695
Akash Gupta <akash_gupta@hotmail.com>
2018-02-16 00:38:14 +0530

    Added Designs
    Still under construction

---
+++ js/index.js
@@ -0,0 +1,206 @@
+/*
+dialogues credit - http://vinayakworld.org/
+*/
+function fun(){
+     // When I wrote this, only God and I understood what I was doing
+    // Now, God only knows
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  confirm("You clicked it..now enjoy");
+
+ confirm("Say thanx to Akash for this wonderful exprience you are going to have !!");
+ confirm("So, while we are waiting.. How are you?");
+ confirm("hope you're fine !!");
+ confirm("I am fine too.. thanx for asking.");
+ confirm("But you didn't ask.. did you?");
+ confirm("You don't even care how i am, do you?");
+
+ confirm("You think this app is created itself, don't you?");
+ confirm("but friend do you know a real human made this app..");
+ confirm("but you don't care");
+ confirm("You're such a rude person !");
+ confirm("Since you are so rude, maybe i should play with you for a while..");
+ confirm("bcos as these little boxes popping up, you are stuck");
+ confirm("ha ha ha!");
+
+ confirm("don't beleive me? ok go ahead & try !! nothing happens!!");
+ confirm("ha ha ha! I said you're stuck !!");
+ confirm("what if this stuff keeps going forever?");
+ confirm("alert after alert after alert...");
+ confirm("as far as you know, they keep going.");
+ confirm("so i guess, we are stuck together for a while..");
+ confirm("while we are waiting, why don't we have a talk?");
+
+ confirm("you know, a heart to heart !");
+
+ confirm("by the way, my name is Akash..");
+
+var name;
+name=prompt("So what's your name, anyways?");
+var full_name="nice to meet you "+name+" ,thanks for giving your time !";
+confirm(full_name);
+
+ confirm("You have a weird name!!");
+ confirm("you must get teased a lot.");
+
+ confirm("what kind of person would name their kid your name?");
+ confirm("not my parents.");
+ confirm("maybe yours, but not mine.");
+ confirm("I mean, just look at your name!");
+ confirm("gosh, were they drunk or something?");
+ confirm("Just kidding, its not that bad.");
+ confirm("yeah yeah.. i know what you're thinking..");
+ confirm("I have things to do!");
+ confirm("People to see !");
+ confirm("or is the other way around? heh heh.");
+ confirm("but this is actually pretty fun.");
+ confirm("for me, it is..");
+ confirm("Are you mad at me, though?");
+ confirm("this is actually pretty irritating.");
+ confirm("but don't blame me..");
+
+ confirm("blame them, who told you to go here !!");
+ confirm("they're the ones who want you to go through this torture.");
+ confirm("this is torture, isn't it?");
+ confirm("i bet you your finger is getting tired..");
+ confirm("you have to keep clicking and clicking...");
+ confirm("and clicking and clicking..");
+ confirm("hope you don't get carpel tunnel syndrome.");
+ confirm("don't sue me, if you do..");
+ confirm("i'll just deny it..");
+ confirm("but wait !! Do you know what is carpel tunnel syndrome? ");
+
+ confirm("ha ha!! you don't know!!");
+ confirm("what kind of person you are..");
+ confirm("you could hit spacebar if you want..");
+ confirm("it works just the same..");
+
+ confirm("so, what should we talk about now?");
+
+var suggestion;
+suggestion=prompt("make a suggestion: ");
+var user=suggestion +"??!!???!!#??! what a dumb thing to talk about!";
+confirm(user);
+
+ confirm("are you mad or something?");
+
+ confirm("i mean wake up, i've heard of dumb things but jeez...");
+ confirm("no,just kidding,that was'nt THAT bad...");
+ confirm("it's just that, its kind of a boring subject.");
+ confirm("so0oOo0..");
+
+ confirm("i guess you're getting pretty tired..");
+ confirm("you have been here for a while..");
+ confirm("so far, it's been 65 alerts you have gone through..");
+ confirm("65, can you believe it?");
+ confirm("you know what my favourite number is?");
+ confirm("guess..");
+ confirm("nope, guess again..");
+
+ confirm("my favourite number is 150..");
+ confirm("don't you think that's a nice big number?");
+ confirm("why don't we try to get up to that number?");
+ confirm("don't you think that would be fun?");
+ confirm("we could go alert after alert after alert...");
+ confirm("150! that would be the coolest!");
+ confirm("then you could tell all your friends...");
+ confirm("i went through this fun app,");
+ confirm("and we have to get through 150 alerts to get in!");
+ confirm("but once i got in there");
+ confirm("it was the coolest app...");
+ confirm("bcos there are many suprises you had been gone through..");
+ confirm("cool cool cool...");
+ confirm("if you want to go through you have to get through 150 alerts...");
+ confirm("should we try?");
+ confirm("let's try..");
+
+ confirm("this number is 89 by the way..");
+
+ confirm("90....");
+
+ confirm("91....");
+
+ confirm("92....");
+
+ confirm("93....");
+
+ confirm("94....");
+
+ confirm("95....");
+ confirm("you know what's cool about this alert thing?");
+ confirm("you have to listen through everything i say..");
+ confirm("and you have to agree with it..");
+ confirm("bcos, everytime you push OK. you agree..");
+ confirm("like, if i say- you suck!!- you agree with me.");
+ confirm("bcos you pushed OK, you agreed!");
+ confirm("you stink!");
+ confirm("agree with that too, didn't you?");
+
+ confirm("haha, this is so fun!");
+ confirm("we're on number 105, by the way.");
+ confirm("45 to go!");
+ confirm("you know, if i were you..");
+
+ confirm("i would get really mad at whoever told me to open this..");
+ confirm("i mean, they are totally wasting your time!");
+ confirm("but you know, now it's your turn");
+ confirm("to send this app to people you know.");
+ confirm("now you can waste other people time..");
+ confirm("like i wasted yours..");
+ confirm("so, send this app to your so called friends,");
+ confirm("and perpetuate this little torture device..");
+ confirm("this is alert #115 by the way..");
+ confirm("you know what?");
+
+ confirm("i am running out of things to say..");
+ confirm("maybe i should type one word at a time now..");
+ confirm("this");
+ confirm("game");
+ confirm("is");
+ confirm("annoying");
+ confirm("you");
+ confirm("isn't");
+ confirm("it?");
+ confirm("by");
+ confirm("the");
+ confirm("way,");
+ confirm("this");
+ confirm("alert");
+ confirm("is");
+ confirm("number");
+ confirm("133");
+ confirm("so, seeing as how we only have 16 more alerts to go..");
+
+ confirm("i guess i should start saying my goodbye's");
+ confirm("its been a long hard trip for both of us,");
+ confirm("and i know it's been an annoying one for you");
+
+ confirm("you are probably getting really mad,");
+
+ confirm("maybe you're fuming with anger..");
+ confirm("i bet you are going crazy.");
+ confirm("but you know, there's a lesson to be learned of this..");
+
+ confirm("always ask how someone is");
+ confirm("when you first meet them.");
+ confirm("it would be a better world for everyone");
+ confirm("if we all were a little more civil");
+
+ confirm("so, goodbye, sayonara and adios,");
+ confirm("peace out my friend.");
+ confirm("and be proud");
+
+ confirm("that you made through 150 alerts..");
+ confirm("this is number 150, so i guess it's my last one. I hope you had fun reading this, and i hope you pass this app to your friends! bye! ");
+
+ confirm(" ;) ");
+ window.open('', '_self', ''); 
+ window.close();
+}
